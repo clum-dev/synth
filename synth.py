c2fc249c8618893a4271d8715159d5561cbd3fd4
Calum Davis <calum.davis545@gmail.com>
2023-08-22 23:55:58 +1000

    added unison voices to synth added ability to program notes via excel (see notes.xlsx for example)

--- synth.py
+++ synth.py
@@ -1,16 +1,19 @@
 import math
 import numpy as np
+import pandas as pd
 import pyaudio
 import itertools
 from operator import itemgetter
 from scipy import interpolate
 from scipy.signal import butter, lfilter
 from scipy.signal import sawtooth, square
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+from sys import maxsize as INTEGER_MAX
 
 
 SAMPLE_RATE = 44100
 DIVS = 16
+MASTER_VOL = 4.0
 
 
 class Note:
@@ -22,6 +25,7 @@ class Note:
         if isinstance(note, int):
             self.index = note
             self.note = Note.NOTES[note]
+
         elif isinstance(note, str):
             self.note = note.strip().lower()
             self.index = Note.NOTES.index(self.note)
@@ -38,7 +42,7 @@ class Note:
         return self.frequency()
 
 
-# TODO: chord player
+# TODO: chord player based on a chord???
 class Scale:
 
     intervals = {'major': [2, 2, 1, 2, 2, 2, 1], 
@@ -93,8 +97,9 @@ class Osc:
     def tri(frequency, samples:int):
         factor = float(frequency) * (math.pi * 2) / SAMPLE_RATE
         return sawtooth(np.arange(samples) * factor, width=0.5)
-    
 
+
+# TODO: fix tempo to actually give correct time divisions
 class Tempo:
     def __init__(self, bpm:int) -> None:
         self.bpm = bpm
@@ -103,19 +108,13 @@ class Tempo:
         # time of div in seconds
         self.divTime = self.hz / DIVS
 
-    def get_samples(self, divCount:int) -> int:
-        return SAMPLE_RATE * self.divTime * divCount
+    # Return the number samples for the given divcount
+    def get_samples(self, divcount:int) -> int:
+        return SAMPLE_RATE * self.divTime * divcount
     
-    def get_samples_time(self, time:float) -> int:
-        
-        # # debug
-        # print("time:", time)
-        # print("div time:", self.divTime)
-        # print("bpm:", self.bpm)
-        # print("hz:", self.hz)
-        # print("test:", (time / self.hz) * DIVS)
-
-        return self.get_samples((time / self.hz) * DIVS)
+    # Return the time elapsed for the given divcount
+    def get_time(self, samples:int) -> float:
+        return samples / SAMPLE_RATE
         
 
 class ADSR:
@@ -169,156 +168,223 @@ class ADSR:
 class Filter:
     def __init__(self, cutoff:float, bType:str='low') -> None:
         assert 0 < cutoff < 1
-        self.b, self.a = butter(8, cutoff, btype=bType)
+        self.b, self.a = butter(16, cutoff, btype=bType)
     
     def apply(self, data):
         return lfilter(self.b, self.a, data)
 
 
 class Synth:
-    def __init__(self, wave, harm:int, adsr:list, tempo:Tempo, cutoff:int, volume:float) -> None:
+
+    # vtype = harmonic | unison
+    def __init__(self, name:str, wave, voices:int, vtype:str, adsr:list, tempo:Tempo, cutoff:int, volume:float) -> None:
+        self.name = name
         self.wave = wave
-        self.harm = max(0, harm)
-        self.volume = max(0, volume)
+        self.voices = max(0, voices)
+        self.vtype = vtype
+        self.volume = max(0, volume) * MASTER_VOL
         self.adsr = ADSR(adsr)
-        self.filt = Filter(cutoff)
+        self.filt = Filter(ADSR.clamp(cutoff, 0.1, 0.9))
         self.tempo = tempo
 
-    # TODO: change to unison voices instead of harmonics
-    def harmonics(self, freq, time):
-        harms = []
-        for i in range(self.harm + 1):
-            freqMod = 2**(i+1)
-            volMod = 0.5**(i+1)
-            # print("freqmod:", freqMod)
-            # print("volmod:", volMod)
-            
-            harms.append(self.wave(freq * freqMod, self.tempo.get_samples_time(time)) * self.volume * volMod)
-        return sum(harms)
+    def unison(self, freq, samp):
+        if self.voices == 0:
+            return self.wave(freq, samp) * self.volume * 0.5
+        
+        unison = []
+        
+        spreadMod = 1
+        polarity = 1
 
-        # single
-        # return self.wave(freq, self.tempo.get_samples_time(time)) * self.volume
-    
-    def harmonics_samp(self, freq, samp):
+        for i in range(self.voices):
+            freqMod = freq * 0.003 * spreadMod
+            volMod = 0.2
+            # print(f'freq: {freq}\tfreq mod: {freqMod}\tvoice: {freq + (freqMod * polarity)}')
+            unison.append(self.wave(freq + (freqMod * polarity), samp) * self.volume * volMod)
+
+            if i % 2 == 0:
+                spreadMod += 1
+            polarity *= -1
+
+        return sum(unison)
+        
+    def harmonics(self, freq, samp):
+        if self.voices == 0:
+            return self.wave(freq, samp) * self.volume * 0.5
+        
         harms = []
-        for i in range(self.harm + 1):
+        for i in range(self.voices + 1):
             freqMod = 2**(i+1)
             volMod = 0.5**(i+1)
-            # print("freqmod:", freqMod)
-            # print("volmod:", volMod)
-            
             harms.append(self.wave(freq * freqMod, samp) * self.volume * volMod)
+
         return sum(harms)
-
-    def gen_signal(self, freq, time):
-        chunk = self.harmonics(freq, time)
-        chunk = self.filt.apply(chunk)
-        return self.adsr.gen_envelope(chunk, time)
     
-    def gen_signal_samp(self, freq, time, samp):
-        chunk = self.harmonics_samp(freq, samp)
+    def gen_signal(self, freq, samp):
+        if self.vtype == 'harmonic':
+            chunk = self.harmonics(freq, samp)
+        elif self.vtype == 'unison':
+            chunk = self.unison(freq, samp)
+
         chunk = self.filt.apply(chunk)
-        return self.adsr.gen_envelope(chunk, time)
+        return self.adsr.gen_envelope(chunk, self.tempo.get_time(samp))
 
 
-#===============================================================
+class NoteGenerator:
+    def __init__(self, filepath:str, sheetname:str, t:Tempo) -> None:
+        self.data = pd.read_excel(filepath, sheet_name=sheetname)
+        self.silence = Synth(name='silence',
+                             wave=Osc.silence,
+                             voices=0,
+                             vtype='harmonic',
+                             adsr=[0.0, 0.0, 0.0, 0.0],
+                             tempo=t,
+                             cutoff=0.9,
+                             volume=0.2
+                             )
+        self.t = t
+
+    # return note, oct
+    def parse(self, note:str):
+        if len(note) == 2:
+            return note[0], int(note[1])
+        elif len(note) == 3:
+            return note[0:2], int(note[2])
+
+        else:
+            return None, None
+    
+
+    def gen_notes(self, col:pd.DataFrame, synth:Synth) -> list:
+        
+        # col = self.data.loc[:,colName]        # OLD
+        out = []
+        divcount = 1
+        current:Note = None
+
+        i = 0
+        while i < len(col):
+            # Append silence (empty cell)
+            if str(col[i]) == 'nan':
+                i += 1
+                out.append(self.silence.gen_signal(0, self.t.get_samples(1)))
+                continue
+                
+            # Get note / octave
+            note, oct = self.parse(col[i])
+            # print(f'note:{note}\toct:{oct}')
+
+            if note != None:
+                current = Note(note, oct)
+
+            # Get sustain length
+            i += 1
+            while i < len(col) and col[i] == '|':
+                divcount += 1
+                i += 1
+                
+            out.append(synth.gen_signal(current.frequency(), self.t.get_samples(divcount)))
+
+            # Reset divcount
+            divcount = 1
+
+        # Organise output
+        out = np.concatenate(out).flatten()
+        sampLen = len(out)
+        out = out.reshape(1, sampLen)
+
+        return out, sampLen
+    
+
+    def get(self, synths:list) -> list:
+        chunks = []
+        shortest = INTEGER_MAX
+
+        # Generate notes
+        for synth in synths:
+            assert isinstance(synth, Synth)
+            
+            # Get all instrument instances by name
+            cols = [col for col in self.data if col.startswith(synth.name)]
+            for c in cols:
+                notes, sampLen = self.gen_notes(self.data[c], synth)
+                shortest = min(shortest, sampLen)
+
+                # chunks.append(notes)
+                chunks.append(notes * (1 / len(cols)))  # add with normalized volume
+
+        # Equalize lengths and sum signals
+        temp = []
+        for c in chunks:
+            temp.append(np.asarray(c).flatten()[:shortest].reshape(1, shortest))
+        
+        # debug
+        print(np.shape(temp))
+
+        return sum(temp)
 
 
-t = Tempo(120)
-s = Synth(Osc.sine, 
-          harm=1,
-          adsr=[0.01, 0.0, 1.0, 0.01],
-          tempo=t,
-          cutoff=0.7,
-          volume=0.2
-          )
+def main():
+    tempo = Tempo(80)
+    notegen = NoteGenerator(filepath="notes.xlsx", 
+                            sheetname='Demo', 
+                            t=tempo)
 
-b = Synth(Osc.saw,
-          harm=0,
-          adsr=[0.01, 0.1, 0.1, 0.01],
-          tempo=t,
-          cutoff=0.9,
-          volume=0.4          
-          )
+    # Lead synth
+    pad = Synth(name='pad',
+                wave=Osc.saw,
+                voices=3,
+                vtype='unison',
+                adsr=[1.3, 0.3, 1.0, 0.3],
+                tempo=tempo,
+                cutoff=0.8,
+                volume=0.3
+                )
 
-l = Synth(Osc.square,
-          harm=1,
-          adsr=[0.7, 0.0, 1.0, 0.01],
-          tempo=t,
-          cutoff=0.9,
-          volume=0.2
-          )
+    # Bass synth
+    bass = Synth(name='bass',
+                wave=Osc.saw,
+                voices=2,
+                vtype='unison',
+                adsr=[0.01, 0.5, 0.1, 0.05],
+                tempo=tempo,
+                cutoff=0.4,
+                volume=0.17
+                )
 
-r = Synth(Osc.silence,
-          harm=0,
-          adsr=[0.0, 0.0, 0.0, 0.0],
-          tempo=t,
-          cutoff=0.9,
-          volume=0.2
-          )
+    # Lead synth
+    lead = Synth(name='lead',
+                wave=Osc.tri,
+                voices=2,
+                vtype='unison',
+                adsr=[0.01, 0.0, 1.0, 0.05],
+                tempo=tempo,
+                cutoff=0.9,
+                volume=0.3
+                )
 
-CM = Scale(Note('C', octave=4), 'major')
-Cm = Scale(Note('C#', octave=4), 'minor')
+    # Arp synth
+    arp = Synth(name='arp',
+                wave=Osc.square,
+                voices=1,
+                vtype='unison',
+                adsr=[0.02, 0.03, 0.4, 0.1],
+                tempo=tempo,
+                cutoff=0.6,
+                volume=0.12
+                )
 
-pads = []
-bass = []
-lead = []
+    chunks = notegen.get([pad, bass, lead, arp])
 
-# Pads
-notelen = 4
-pads.append(s.gen_signal(Note('C', octave=3).frequency(), notelen) + 
-            s.gen_signal(Note('E', octave=3).frequency(), notelen) + 
-            s.gen_signal(Note('G', octave=3).frequency(), notelen) + 
-            s.gen_signal(Note('C', octave=4).frequency(), notelen))
-
-# Bass
-for _ in range(8):
-    bass.append(b.gen_signal(Note('C', octave=1).frequency(), notelen/16))
-    bass.append(b.gen_signal(Note('C', octave=2).frequency(), notelen/16))
-
-# Lead
-lead.append(l.gen_signal(Note('C', octave=3).frequency(), 0.5))
-lead.append(l.gen_signal(Note('E', octave=3).frequency(), 0.5))
-lead.append(l.gen_signal(Note('G', octave=3).frequency(), 0.5))
-lead.append(l.gen_signal(Note('C', octave=3).frequency(), 2.5) + 
-            l.gen_signal(Note('C', octave=4).frequency(), 2.5))
-
-lead = np.concatenate(lead)
-lead = lead.reshape(1, len(lead))
-
-
-# Combine signals
-bass = np.array(bass).flatten()
-bass = bass.reshape(1, len(bass))
-
-print("pads", np.shape(pads))
-print("bass", np.shape(bass))
-print("lead", np.shape(lead))
-
-shortest = min(np.array(pads).size, np.array(bass).size, np.array(lead).size)
-print("shortest", shortest)
-
-pads = np.asarray(pads).flatten()[:shortest].reshape(1, shortest)
-bass = np.asarray(bass).flatten()[:shortest].reshape(1, shortest)
-lead = np.asarray(lead).flatten()[:shortest].reshape(1, shortest)
-
-print('-'*20)
-print("pads", np.shape(pads))
-print("bass", np.shape(bass))
-print("lead", np.shape(lead))
-
-# Add together
-chunks = pads + bass + lead
-
-
-# Play stuff
-chunk = np.concatenate(chunks) * 0.25
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paFloat32, channels=1, rate=SAMPLE_RATE, output=1)
-stream.write(chunk.astype(np.float32).tobytes())
-stream.close()
-p.terminate()
+    # Play stuff
+    chunk = np.concatenate(chunks) * 0.25
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paFloat32, channels=1, rate=SAMPLE_RATE, output=1)
+    stream.write(chunk.astype(np.float32).tobytes())
+    stream.close()
+    p.terminate()
 
 
 
-
+if __name__ == "__main__":
+    main()
